b848fc876adc717e041fd0cf44a0b222b06e4cd1
csuaco <christian.suacom@optum.com>
2021-01-18 15:54:51 +0000

    Added HelloLYIT.java

---
+++ HelloLYIT.java
@@ -0,0 +1,11 @@
+public class HelloLYIT{
+
+	public static void main(String[] args) {
+	
+		// Print "Hello, LYIT from Ian" as output
+		
+		System.output.println("Hello, LYIT from Ian");
+		
+	}
+	
+}
